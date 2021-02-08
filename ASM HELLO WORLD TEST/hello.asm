2641ad550e932ea4e9e2cfc7d469046a8e4e52c8
rrTio <rozsraventio2305@gmail.com>
2021-02-08 02:01:20 -0500

    ASSEMBLY CODE TEST RUN

--- ASM HELLO WORLD TEST/hello.asm
+++ ASM HELLO WORLD TEST/hello.asm
@@ -1,11 +1,12 @@
 section .data
 	text db "Hello, World!", 10
+	new db "TEST", 10
 section .text
 	global _start
 _start: 
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, text
+	mov rsi, text, new
 	mov rdx, 14
 	syscall
 
---
+++ ASM/A to E.asm
@@ -0,0 +1,42 @@
+cseg segment para 'code'
+assume cs:cseg;ds:cseg;ss:cseg;es:cseg
+org 100h
+start:
+mov ax,0003h; clrscr
+int 10h
+;A
+mov ah,02;gotoxy
+mov dh,11
+mov dl,39
+int 10h
+mov ah,02;print char
+mov dl,'A'
+int 21h
+;B
+mov ah,02;gotoxy
+mov dh,0
+mov dl,79
+int 10h
+mov ah,02;print char
+mov dl,'B'
+int 21h
+;C
+mov ah,02;gotoxy
+mov dh,23
+mov dl,79
+int 10h
+mov ah,02;print char
+mov dl,'E'
+int 21h
+;D
+mov ah,02;gotoxy
+mov dh,23
+mov dl,0
+int 10h
+mov ah,02;print char
+mov dl,'D'
+int 21h
+
+int 20h
+cseg ends
+end start
