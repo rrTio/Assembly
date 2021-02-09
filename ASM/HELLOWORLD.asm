3caa5cb336bd7104818d9e8ef5bd7cd7b8746ad2
rrTio <tio.rozsraven@ue.edu.ph>
2021-02-09 01:46:22 -0500

    New Update

--- ASM/HELLOWORLD.asm
+++ ASM/HELLOWORLD.asm
@@ -4,12 +4,12 @@ org 100h
 start:
 mov ax,0003h
 int 10h 
-mov ah,02
-mov dh,11
-mov dl,39
+mov ah,14
+mov dh,00
+mov dl,00
 int 10h
-mov ah,02
-mov dl,'C'
+mov dl,'Hello World' ;print 'Hello World'
+mov ah,14
 int 21h
 cseg ends
 end start
