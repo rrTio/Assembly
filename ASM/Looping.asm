57e49b325fc2b2270748e73c08017edddca17a40
rrTio <tio.rozsraven@ue.edu.ph>
2021-03-04 18:20:27 +0800

    reAssembly

--- ASM/Looping.asm
+++ ASM/Looping.asm
@@ -1,11 +1,11 @@
-g macro r,c
+gotoxy macro r,c
 mov ah,02
 mov dh,r
 mov dl,c
 int 10h
 endm
 
-pc macro x
+printChar macro x
 mov ah,02
 mov dl,x
 int 21h
@@ -15,45 +15,46 @@ cseg segment para 'code'
 assume cs:cseg;ds:cseg;ss:cseg;es:cseg
 org 100h
 start:jmp begin
-n1 db 'A'
-r1 db 7
-n2 db 'A'
-r2 db 0
-c2 db 0
-n3 db 'A'
-r3 db 0
-c3 db 23
+RightChar db 'A'
+rightCharRow db 7 ;row 7
+characterLeft db 'A'
+leftCharacterRow db 0 ;Left Character Row 0
+leftCharacterColumn db 0 ;Left Character Column 0
+characterRight db 'A'
+RightCharacterRow db 0 ; Right Character Row 0
+RightCharacterColumn db 23 ;Right Character Column 23
 begin:
 mov ax,0003h
 int 10h
-;1-8
-mov cx,1
-x:g r1,70
-pc n1
-inc r1
-inc n1
-inc cx
-cmp cx,8
-jbe x
+
+;A-H
+mov cx,0
+x:gotoxy rightCharRow, 50 ;50 == column
+printChar RightChar
+inc rightCharRow ;increment rows
+inc RightChar ;increment character
+inc cx ;increment value
+cmp cx,8 ;loop terminator
+jbe x ;check validity
+
 ;====
 ;A-Z
-mov cx,24
-y:g r2,c2
-pc n2
-inc r2
-inc c2
-inc n2
+mov cx,24 ;24 == limit
+y:gotoxy leftCharacterRow,leftCharacterColumn
+printChar characterLeft
+inc leftCharacterRow
+inc leftCharacterColumn
+inc characterLeft
 loop y
 
-mov cx,24
-z:g r3,c3
-pc n3
-inc r3
-dec c3
-inc n3
+mov cx,24 ;24 == limit
+z:gotoxy RightCharacterRow,RightCharacterColumn
+printChar characterRight
+inc RightCharacterRow
+dec RightCharacterColumn
+inc characterRight
 loop z
 
-;g 23,0
 int 20h
 cseg ends
 end start
