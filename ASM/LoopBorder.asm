0221a839b3a8fd2b4569fcfb23b865e9b83e0876
rrTio <tio.rozsraven@ue.edu.ph>
2021-03-04 18:27:29 +0800

    re LoopBorder

--- ASM/LoopBorder.asm
+++ ASM/LoopBorder.asm
@@ -15,48 +15,43 @@ cseg segment para 'code'
 assume cs:cseg;ds:cseg;ss:cseg;es:cseg
 org 100h
 start: jmp begin
-c1 db 0
-r1 db 0
+column db 0
+row db 0
 
 begin:
 mov ax,0003h
 int 10h
 
 mov cx,80
-x1:g 0,c1
-pc 'R'
-inc c1
+x1:g 0,column
+pc 'A'
+inc column
 loop x1
 
 mov cx,24
-x2:g r1,79
-pc 'R'
-inc r1
+x2:g row,79
+pc 'B'
+inc row
 loop x2
 
-dec c1
+dec column
 mov cx,80
-x3:g 23,c1
-pc 'R'
-dec c1
+x3:g 23,column
+pc 'C'
+dec column
 loop x3
 
-dec r1
+dec row
 mov cx,24
-x4:g r1,0
-pc 'R'
-dec r1
+x4:g row,0
+pc 'D'
+dec row
 loop x4
 
 g 11,39
-pc 'R'
+pc 'E'
 
 g 23,0
 int 20h
 cseg ends
 end start
-
-
-
-
-
